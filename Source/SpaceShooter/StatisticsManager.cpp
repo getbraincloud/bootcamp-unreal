16ca58dc24bf8afe6fc8320df310565725b9c3d2
Brad-F <bradleyf@bitheads.com>
2022-11-22 12:44:20 -0500

    Initial commit for Lesson-20

---
+++ Source/SpaceShooter/StatisticsManager.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StatisticsManager.h"
+
+
+void UStatisticsManager::SetStatistics(const std::vector<Statistic>& statistics)
+{
+    m_Statistics = statistics;
+}
+
+Statistic* UStatisticsManager::GetStatisticByName(const FString& name)
+{
+    for (int i = 0; i < m_Statistics.size(); i++)
+        if (m_Statistics[i].GetName() == name)
+            return &m_Statistics[i];
+    return nullptr;
+}
+
+Statistic* UStatisticsManager::GetStatisticAtIndex(int index)
+{
+    if (index >= 0 && index <  m_Statistics.size())
+        return &m_Statistics[index];
+    return nullptr;
+}
+
+int UStatisticsManager::GetCount()
+{
+    return m_Statistics.size();
+}
