7fba5ba58a7a9d4b4faafbd8d6c7967dd83e1aad
Brad-F <bradleyf@bitheads.com>
2022-11-22 12:27:09 -0500

    Initial commit for Lesson-15

---
+++ Source/SpaceShooter/NetworkCallback.h
@@ -0,0 +1,34 @@
+// Copyright 2022 bitHeads, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Leaderboard.h"
+#include <vector>
+
+/**
+ * 
+ */
+class SPACESHOOTER_API NetworkCallback
+{
+public:
+    NetworkCallback() = default;
+    virtual ~NetworkCallback() = default;
+    
+    virtual void OnAuthenticationRequestCompleted() {}
+    virtual void OnAuthenticationRequestFailed(const FString& errorMessage) {}
+    
+    virtual void OnLogOutRequestCompleted() {}
+    virtual void OnLogOutRequestFailed() {}
+    
+    virtual void OnUpdateUsernameRequestCompleted() {}
+    virtual void OnUpdateUsernameRequestFailed() {}
+    
+    virtual void OnLeaderboardRequestCompleted(const Leaderboard& leaderboard) {}
+    virtual void OnLeaderboardRequestFailed() {}
+    
+    virtual void OnPostScoreToLeaderboardRequestCompleted() {}
+    virtual void OnPostScoreToLeaderboardRequestFailed() {}
+    
+    //TODO: Add Network callback methods here
+};
