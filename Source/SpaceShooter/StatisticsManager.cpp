67a84f224096674b77c0cfea5310027b786b9499
Brad-F <bradleyf@bitheads.com>
2022-11-22 13:21:56 -0500

    Initial commit for Lesson-24

---
+++ Source/SpaceShooter/StatisticsManager.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StatisticsManager.h"
+
+
+FString UStatisticsManager::GetFormatedIncrementsData()
+{
+    FString jsonData = TEXT("{");
+    
+    for (int i = 0; i < m_Statistics.size(); i++)
+    {
+        jsonData.Append(TEXT("\"" + m_Statistics[i].GetName() + "\" : " + FString::FromInt(m_Statistics[i].GetIncrement())));
+                
+        if(i != m_Statistics.size() - 1)
+            jsonData.Append(TEXT(","));
+    }
+    
+    jsonData.Append(TEXT("}"));
+    
+    return jsonData;
+}
+
+void UStatisticsManager::SetStatistics(const std::vector<Statistic>& statistics)
+{
+    m_Statistics = statistics;
+}
+
+Statistic* UStatisticsManager::GetStatisticByName(const FString& name)
+{
+    for (int i = 0; i < m_Statistics.size(); i++)
+        if (m_Statistics[i].GetName() == name)
+            return &m_Statistics[i];
+    return nullptr;
+}
+
+Statistic* UStatisticsManager::GetStatisticAtIndex(int index)
+{
+    if (index >= 0 && index <  m_Statistics.size())
+        return &m_Statistics[index];
+    return nullptr;
+}
+
+int UStatisticsManager::GetCount()
+{
+    return m_Statistics.size();
+}
