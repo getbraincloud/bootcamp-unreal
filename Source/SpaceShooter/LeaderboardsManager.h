ae61e3e23d7dc81d7818bc56c69359b62a73b5ba
bitHeads-Brad <bradleyf@bitheads.com>
2023-04-26 14:06:47 -0400

    Updated UE project to 5.1

--- Source/SpaceShooter/LeaderboardsManager.h
+++ Source/SpaceShooter/LeaderboardsManager.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2022 bitHeads, Inc. All Rights Reserved.
 
 #pragma once
 
@@ -6,7 +6,6 @@
 #include "Leaderboard.h"
 #include "LeaderboardEntry.h"
 #include "Constants.h"
-#include <vector>
 #include "UObject/NoExportTypes.h"
 #include "LeaderboardsManager.generated.h"
 
@@ -32,6 +31,6 @@ public:
     void SetUserTime(float userTime);
     
 private:
-    std::vector<Leaderboard> m_Leaderboards;
+    TArray<Leaderboard> m_Leaderboards;
     float m_UserTime;
 };
