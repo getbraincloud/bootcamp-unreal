21b8d48ec7520db5f654b359c9db5726f620be25
bitHeads-Brad <bradleyf@bitheads.com>
2024-03-25 13:15:32 -0400

    Updating brainCloud client to 5.2

---
+++ Plugins/BCClient/Source/BCWidget/Private/BCWidgetPrivatePCH.h
@@ -0,0 +1,17 @@
+// Copyright 2023 bitHeads, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Framework/Commands/UIAction.h"
+#include "LevelEditorMenuContext.h"
+#include "ToolMenus.h"
+#include <LevelEditor.h>
+#include "Editor.h"
+
+#include "Modules/ModuleManager.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogBCWidget, Log, All);
+
+#include "BCWidgetModule.h"
