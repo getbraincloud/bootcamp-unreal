a1d7da2089668551a162ac338cf42042cbeba57e
bitHeads-Brad <bradleyf@bitheads.com>
2024-03-25 13:26:08 -0400

    Updating brainCloud client to 5.2

---
+++ Plugins/BCClient/Source/BCCompatibility/Private/BCCompatibilityPCH.h
@@ -0,0 +1,13 @@
+// Copyright 2023 bitHeads, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HAL/FileManager.h"
+#include "Misc/Paths.h"
+
+#include "Modules/ModuleManager.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogBCCompatibility, Log, All);
+
+#include "BCCompatibilityModule.h"
