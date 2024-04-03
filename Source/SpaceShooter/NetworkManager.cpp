864dbaa0054ad5f9e25911f54f6d89d25575df99
Brad-F <bradleyf@bitheads.com>
2024-04-03 14:27:08 -0400

    Added app and bc client versions to the HUD

--- Source/SpaceShooter/NetworkManager.cpp
+++ Source/SpaceShooter/NetworkManager.cpp
@@ -26,6 +26,11 @@ void UNetworkManager::SetCallback(NetworkCallback* callback)
     m_Callback = callback;
 }
 
+FString UNetworkManager::GetBrainCloudClientVersion()
+{
+    return m_BrainCloud->getClient()->getBrainCloudClientVersion();
+}
+
 bool UNetworkManager::IsAuthenticated()
 {
     return false;
