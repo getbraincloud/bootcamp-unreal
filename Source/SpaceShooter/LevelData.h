0ba51408f551c1a3f397671faae582863c734fe8
Brad-F <bradleyf@bitheads.com>
2024-05-21 11:53:12 -0400

    Fixed issue with global entities not being sorted

--- Source/SpaceShooter/LevelData.h
+++ Source/SpaceShooter/LevelData.h
@@ -53,7 +53,9 @@ public:
     std::vector<EnemyType> GetEnemyTypes();
     std::vector<FString> GetStartingAsteroids();
     std::vector<FString> GetStartingEnemies();
-        
+    
+    static bool Compare(LevelData& a, LevelData& b);
+    
 private:
     std::vector<EnemyType> m_EnemyTypes;
     std::vector<FString> m_StartingAsteroids;
